6230d31e19809d04a240138072a4e242bb723228
Nathaniel Walston <nathaniel.walston@kareo.com>
2020-03-28 10:26:17 -0700

    feat: add the most basic test case

--- __tests__/sign-in.js
+++ __tests__/sign-in.js
@@ -1,8 +1,32 @@
-// import React from "react";
-// import { render, waitForDomChange } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
-// import Form from "../src/index";
+import React from "react";
+import { render, waitForDomChange } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Form from "../src/index";
 
 test("Login Test", async done => {
-  done();
+  const USERNAME = "jerry";
+  const PASSWORD = "jerry";
+  function checkValues(_event, data, _form) {
+    expect(data.values.username).toBe(USERNAME);
+    expect(data.values.password).toBe(PASSWORD);
+    done();
+  }
+
+  const { getByLabelText, getByText } = render(
+    <Form onSubmitWithData={checkValues}>
+      <label htmlFor="username">
+        Username:
+        <input id="username" name="username" type="text" />
+      </label>
+      <label htmlFor="password">
+        Password:
+        <input id="password" name="password" type="password" />
+      </label>
+      <input type="submit" value="Submit" />
+    </Form>
+  );
+
+  await userEvent.type(getByLabelText("Username:"), USERNAME);
+  await userEvent.type(getByLabelText("Password:"), PASSWORD);
+  userEvent.click(getByText("Submit"));
 });
