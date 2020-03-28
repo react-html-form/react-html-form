3d0a02ee9575f78d6db04ade8958eef412886c40
Nathaniel Walston <nathaniel.walston@kareo.com>
2020-03-28 09:58:53 -0700

    feat: install Jest and update Babel

---
+++ __tests__/sign-in.js
@@ -0,0 +1,8 @@
+// import React from "react";
+// import { render, waitForDomChange } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+// import Form from "../src/index";
+
+test("Login Test", async done => {
+  done();
+});
