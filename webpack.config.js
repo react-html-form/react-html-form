5c32c91efab3f443e0918bbaa31101752ae6af40
Nathaniel Walston <nathaniel.walston@kareo.com>
2020-04-04 15:23:35 -0700

    fix: renames to root file to tsx

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   resolve: { extensions: [".js", ".jsx", ".ts", "tsx"] },
   output: {
     path: path.resolve(__dirname, "dist"),
